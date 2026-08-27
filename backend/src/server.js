1810f7464490be5df180ec9f5de3122813e0b29d
Malith Ranathunga <malithranathunga18@gmail.com>
2026-08-27 11:32:20 +0530

    feat(server): mount user, task, and board API routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,11 +2,20 @@ import express from "express"
 import dotenv from "dotenv"
 
 import { connectDB } from "./config/db.js";
+import userRoutes from "./routes/userRoute.js";
+import taskRoutes from "./routes/taskRoute.js";
+import boardRoutes from "./routes/boardRoute.js";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
+
+app.use(express.json())
+
+app.use("/api/users", userRoutes);
+app.use("/api/tasks", taskRoutes);
+app.use("/api/boards", boardRoutes);
 
 connectDB().then(() => {
   app.listen(port, () => {
