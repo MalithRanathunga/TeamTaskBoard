0cd834da3a80fe8f384dc36bf884a03585515102
Malith Ranathunga <malithranathunga18@gmail.com>
2026-08-27 13:57:26 +0530

    feat(frontend): integrate React Router, AuthContext, Axios client, and dynamic TaskBoard

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from "express"
 import dotenv from "dotenv"
+import cors from "cors"
 
 import { connectDB } from "./config/db.js";
 import userRoutes from "./routes/userRoute.js";
@@ -13,6 +14,12 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json())
 
 app.use("/api/users", userRoutes);
